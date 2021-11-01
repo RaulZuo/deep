4d58dccaf44325ba6f8e6c96721c45bbb5aaf853
RaulZuo <zuobin_nju@163.com>
2021-11-02 00:32:23 +0800

    feature: configs and scripts

--- scripts/genconfig.sh
+++ scripts/genconfig.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # 本脚本功能：根据 scripts/environment.sh 配置，生成 DEEP_PROCESS 组件 DEEP_PROCESS 组件 YAML 配置问卷。
-# 示例：genconfig.sh scripts/environment.sh configs/deep-apiserver.yaml
+# 示例：./genconfig.sh scripts/environment.sh configs/deep-apiserver.yaml
 
 env_file="$1"
 template_file="$2"
@@ -9,3 +9,30 @@ template_file="$2"
 DEEP_PROCESS_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 
 source "${DEEP_PROCESS_ROOT}/scripts/lib/init.sh"
+
+if [ $# -ne 2 ];then
+    deep::log::error "Usage: genconfig.sh scripts/environment.sh configs/deep-apiserver.yaml"
+    exit 1
+fi
+
+source "${env_file}"
+
+declare -A envs
+
+set +u
+for env in $(sed -n 's/^[^#].*${\(.*\)}.*/\1/p' ${template_file})
+do
+    if [ -z "$(eval echo \$${env})" ];then
+        iam::log::error "environment variable '${env}' not set"
+        missing=true
+    fi
+done
+
+if [ "${missing}" ];then
+    iam::log::error 'You may run `source scripts/environment.sh` to set these environment'
+    exit 1
+fi
+
+eval "cat << EOF
+$(cat ${template_file})
+EOF"
--- scripts/lib/init.sh
+++ scripts/lib/init.sh
@@ -13,11 +13,11 @@ export GO111MODULE=on
 # The root of the build/dist directory
 DEEP_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd -P)"
 
-source "${IAM_ROOT}/scripts/lib/util.sh"
-source "${IAM_ROOT}/scripts/lib/logging.sh"
-source "${IAM_ROOT}/scripts/lib/color.sh"
+source "${DEEP_ROOT}/scripts/lib/util.sh"
+source "${DEEP_ROOT}/scripts/lib/logging.sh"
+source "${DEEP_ROOT}/scripts/lib/color.sh"
 
-iam::log::install_errexit
+deep::log::install_errexit
 
-source "${IAM_ROOT}/scripts/lib/version.sh"
-source "${IAM_ROOT}/scripts/lib/golang.sh"
+source "${DEEP_ROOT}/scripts/lib/version.sh"
+source "${DEEP_ROOT}/scripts/lib/golang.sh"
