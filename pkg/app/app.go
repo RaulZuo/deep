e75eac36641d84787b5a4fe94d845e5f5da7177f
RaulZuo <zuobin_nju@163.com>
2021-11-07 23:41:13 +0800

    feature: add insecure_serving

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -12,6 +12,47 @@ import (
 	"os"
 )
 
+var (
+	progressMessage = color.GreenString("==>")
+	//nolint: deadcode,unused,varcheck
+	usageTemplate = fmt.Sprintf(`%s{{if .Runnable}}
+  %s{{end}}{{if .HasAvailableSubCommands}}
+  %s{{end}}{{if gt (len .Aliases) 0}}
+
+%s
+  {{.NameAndAliases}}{{end}}{{if .HasExample}}
+
+%s
+{{.Example}}{{end}}{{if .HasAvailableSubCommands}}
+
+%s{{range .Commands}}{{if (or .IsAvailableCommand (eq .Name "help"))}}
+  %s {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableLocalFlags}}
+
+%s
+{{.LocalFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasAvailableInheritedFlags}}
+
+%s
+{{.InheritedFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasHelpSubCommands}}
+
+%s{{range .Commands}}{{if .IsAdditionalHelpTopicCommand}}
+  {{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableSubCommands}}
+
+Use "%s --help" for more information about a command.{{end}}
+`,
+		color.CyanString("Usage:"),
+		color.GreenString("{{.UseLine}}"),
+		color.GreenString("{{.CommandPath}} [command]"),
+		color.CyanString("Aliases:"),
+		color.CyanString("Examples:"),
+		color.CyanString("Available Commands:"),
+		color.GreenString("{{rpad .Name .NamePadding }}"),
+		color.CyanString("Flags:"),
+		color.CyanString("Global Flags:"),
+		color.CyanString("Additional help topics:"),
+		color.GreenString("{{.CommandPath}} [command]"),
+	)
+)
+
 // App is the main construct of a cli application.
 // It is recommended that an app be created with the app.NewApp() function.
 type App struct {
@@ -193,19 +234,14 @@ func (a *App) runCommand(cmd *cobra.Command, args []string) error {
 	return nil
 }
 
-//func (a *App) Run()  {
-//	if err := a.cmd.Execute(): err != nil {
-//		fmt.Println(""),
-//	}
-//}
-
 func (a *App) applyOptionRules() error {
 	// TODO
 	return nil
 }
 
 func printWorkingDir() {
+	wd, _ := os.Getwd()
 	// TODO
-	//wd, _ := os.Getwd()
+	fmt.Println("%v WorkingDir: %s", progressMessage, wd)
 	//log.Infof("%v WorkingDir: %s", progressMessage, wd)
 }
