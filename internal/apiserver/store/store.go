bae0ebb266f7c21a80ec4258880c5be6c7179c81
RaulZuo <zuobin_nju@163.com>
2021-11-12 02:11:50 +0800

    feature: add mysql and store

---
+++ internal/apiserver/store/store.go
@@ -0,0 +1,22 @@
+package store
+
+var client Factory
+
+// Factory defines the iam platform storage interface.
+type Factory interface {
+	// TODO
+	//Users() UserStore
+	//Secrets() SecretStore
+	//Policies() PolicyStore
+	Close() error
+}
+
+// Client return the store client instance.
+func Client() Factory {
+	return client
+}
+
+// SetClient set the iam store client.
+func SetClient(factory Factory) {
+	client = factory
+}
