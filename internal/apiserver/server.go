f6e424f5c3eff02bf0651303d041af51ad0f99e7
RaulZuo <zuobin_nju@163.com>
2021-11-12 02:02:45 +0800

    feature: add mysql start

--- internal/apiserver/server.go
+++ internal/apiserver/server.go
@@ -2,6 +2,7 @@ package apiserver
 
 import (
 	"github.com/RaulZuo/deep/internal/apiserver/config"
+	genericoptions "github.com/RaulZuo/deep/internal/pkg/options"
 	genericapiserver "github.com/RaulZuo/deep/internal/pkg/server"
 )
 
@@ -12,9 +13,19 @@ type apiServer struct {
 type preparedAPIServer struct {
 	*apiServer
 }
+// ExtraConfig defines extra configuration for the deep-apiserver.
+type ExtraConfig struct {
+	Addr		 string
+	mysqlOptions *genericoptions.MySQLOptions
+}
 
 func createAPIServer(cfg *config.Config) (*apiServer, error) {
 	genericConfig, err := buildGenericConfig(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	extraConfig, err := buildExtraConfig(cfg)
 	if err != nil {
 		return nil, err
 	}
@@ -23,6 +34,8 @@ func createAPIServer(cfg *config.Config) (*apiServer, error) {
 	if err != nil {
 		return nil, err
 	}
+	// 配置其他选项，例如数据库等
+	extraConfig.complete().New()
 
 	server := &apiServer{
 		genericAPIServer: genericServer,
@@ -64,6 +77,25 @@ func (s preparedAPIServer) Run() error {
 	return s.genericAPIServer.Run()
 }
 
+type completedExtraConfig struct {
+	*ExtraConfig
+}
+
+// Complete fills in any fields not set that are required to have valid data and can be derived from other fields.
+func (c *ExtraConfig) complete() *completedExtraConfig {
+	if c.Addr == "" {
+		c.Addr = "127.0.0.1:8081"
+	}
+
+	return &completedExtraConfig{c}
+}
+
+func (c *completedExtraConfig) New() (interface {}, error) {
+	//storeIns, _ := mysql.GetMySQLFactoryOr(c.mysqlOptions)
+	//store.SetClient(storeIns)
+	return nil, nil
+}
+
 func buildGenericConfig(cfg *config.Config) (genericConfig *genericapiserver.Config, lastErr error) {
 	genericConfig = genericapiserver.NewConfig()
 	if lastErr = cfg.GenericServerRunOptions.ApplyTo(genericConfig); lastErr != nil {
@@ -84,4 +116,10 @@ func buildGenericConfig(cfg *config.Config) (genericConfig *genericapiserver.Con
 	}
 
 	return
+}
+
+func buildExtraConfig(cfg *config.Config) (*ExtraConfig, error) {
+	return &ExtraConfig{
+		mysqlOptions: cfg.MySQLOptions,
+	}, nil
 }
